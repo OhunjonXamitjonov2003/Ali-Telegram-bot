6ca8de2efdaced2d3dc22cf7c129ad337f29b098
OhunjonXamitjonov2003 <ohunjonxamidjonov@gmail.com>
2024-03-01 15:18:16 +0500

    bot.py fayilini qoshdim

---
+++ bot.py
@@ -0,0 +1,7 @@
+from telebot import TeleBot
+
+bot = TeleBot("7199789473:AAF81VG0KjDgiS5mcIbW9ef7jpNnioxI5jQ")
+
+
+
+
---
+++ main.py
@@ -0,0 +1,23 @@
+# from collections import namedtuple
+# #
+# # User = namedtuple("User","id name lastname age email")
+# # users = [
+# #     (1,"Tohi","Tohirov",20,"tohi@gmail.com"),
+# #     (2,"Tohi","Valiyev",21,"aliyev@gmail.com"),
+# #     (3,"Sobir","Tohirov",22,"sobir@gmail.com")
+# # ]
+# #
+# # for user in users:
+# #     us = User(*user)
+# #     print(us.id,us.name,us.lastname,us.age,us.email)
+# #       print(*us)
+# Car = namedtuple("Car","name color tezligi price orindiqlar_soni boshqaruvi")
+#
+# car = ("Malibu","red","300km/h",20000,4,"Avtomat")
+#
+#
+# ca = Car(*car)
+# print(*ca)
+
+
+
